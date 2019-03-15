51bbff0db943cff558302226f6208337f5e86f98
Jacob Majors <jacobleemajors@gmail.com>
2019-03-14 23:58:04 -0500

    updated

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (id, burger_name, devoured) VALUES ('1', 'Buffalo burger', false);
+INSERT INTO burgers (id, burger_name, devoured) VALUES ('2', 'Patty melt', false);
+INSERT INTO burgers (id, burger_name, devoured) VALUES ('3', 'Jalapeno and cheese', false);
